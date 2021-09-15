c246778bd08a4f7515ae4a4df39f9e533c69c711
nick <nmcamp87@gmail.com>
2021-09-14 22:04:53 -0400

    Added decision class

---
+++ P2/Decision.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    public enum DecisionType
+    {
+        Cancel,
+        Throw,
+        Keep
+    }
+
+    class Decision
+    {
+        private DecisionType type;
+        public Decision(string choice)
+        {
+            type = ParseChoice(choice);
+        }
+
+        private static DecisionType ParseChoice(string choice)
+        {
+            return (DecisionType)Enum.Parse(typeof(DecisionType), choice);
+        }
+
+        public string GetOutput()
+        {
+            string result = null;
+            switch (type)
+            {
+                case DecisionType.Cancel:
+                    result = "cancel.";
+                    break;
+                case DecisionType.Throw:
+                    result = "is to throw it back.";
+                    break;
+                case DecisionType.Keep:
+                    result = "is to keep it.";
+                    break;
+            }
+            return result;
+        }
+    }
+}
